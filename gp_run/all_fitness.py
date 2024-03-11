f5dc9a543b9d8e52d12aca08753631c7ee58528e
wukan <wu-kan@163.com>
2024-03-11 09:23:24 +0800

    转格式并存为excel

--- gp_run/all_fitness.py
+++ gp_run/all_fitness.py
@@ -1,7 +1,15 @@
 import pickle
 from pprint import pprint
 
+import pandas as pd
+
 with open(f'../log/fitness_cache.pkl', 'rb') as f:
     fitness_results = pickle.load(f)
 
 pprint(fitness_results)
+
+# 转成DataFrame
+df = pd.DataFrame.from_dict(fitness_results, orient='index').reset_index()
+print(df)
+
+df.to_excel(f'../log/fitness_cache.xlsx')
