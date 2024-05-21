9a81c52aedf2cc7a02d394423304f9e14a226da8
wukan <wu-kan@163.com>
2024-05-21 17:20:06 +0800

    Update step6.py

--- research/step6.py
+++ research/step6.py
@@ -1,6 +1,7 @@
 """
 读取指定数据文件中的多个特征，生成多个报表
 """
+from datetime import datetime
 
 import polars as pl
 from expr_codegen.tool import codegen_exec
@@ -19,38 +20,44 @@ from sympy_define import *  # noqa
 INPUT1_PATH = r'M:\data3\T1\feature1.parquet'
 # 特征数据文件
 INPUT2_PATH = r'M:\data3\T1\feature2.parquet'
-# 行情信息
+# 行情信息，只跳过了停牌，其它都不能跳过，否则计算收益会出错
 df1 = pl.read_parquet(INPUT1_PATH, columns=['date', 'asset', 'OPEN', 'CLOSE'], use_pyarrow=True)
-# 特征
-df2 = pl.read_parquet(INPUT2_PATH, columns=['date', 'asset', 'DOJI', 'F_11'], use_pyarrow=True)
-df2 = df2.filter(pl.col('F_11').is_not_null(), ~pl.col('DOJI'))
+# 特征。由于过滤了票池以及其它条件，所以数据长度相对短
+df2 = pl.read_parquet(INPUT2_PATH, columns=['date', 'asset', 'NEXT_DOJI', 'A_0007'], use_pyarrow=True).rename({'A_0007': 'FACT'})
 
-
-def __code_block_1():
-    FACTOR = ts_delay(F_11, 1)
-
-
-df2 = codegen_exec(df2, __code_block_1)
+df2 = df2.filter(pl.col('FACT').is_not_null())
 # 由于因子值可能重复，导致前10选出的值是随机的，所以这里还多加了按代码排序，实际情况可能还要考虑其它指标
-df2 = df2.group_by('date').map_groups(lambda x: x.sort(['FACTOR', 'asset'], descending=[True, False]).head(10))
+df2 = df2.group_by('date').map_groups(lambda x: x.sort(['FACT', 'asset'], descending=[True, False]).head(20))
+
+# 过滤第二天涨跌停
+df2 = df2.filter(~pl.col('NEXT_DOJI'))
+
 df = df1.join(df2, on=['date', 'asset'], how='left')
 del df1
 del df2
 
+
+def __code_block_1():
+    FACTOR = ts_delay(FACT, 1)
+
+
+df = codegen_exec(df, __code_block_1)
+# 只观察最近的结果
+df = df.filter(pl.col('date') >= datetime(2024, 1, 1))
 df = df.select(pl.col('date'), pl.col('asset'),
                pl.col('OPEN').alias('fill_price'), pl.col('CLOSE').alias('last_price'),
-               pl.col('FACTOR').is_not_null().cast(pl.Int64).alias('size'))
+               pl.col('FACTOR').is_not_null().cast(pl.Int64).alias('size'),
+               pl.lit(SizeType.TargetValueScale).alias('size_type'))
 df = df.to_pandas()
-df['size_type'] = SizeType.TargetValueScale
 
-df.to_parquet('tmp.parquet')
-df = pd.read_parquet('tmp.parquet')
+# df.to_parquet('tmp.parquet')
+# df = pd.read_parquet('tmp.parquet')
 _K = df['asset'].nunique()
 _N = df['date'].nunique()
-asset = df['asset'].unique()
+asset = sorted(df['asset'].unique())
 
 config = pd.DataFrame({'asset': asset, 'mult': 1.0, 'margin_ratio': 1.0,
-                       'commission_ratio': 0.0005, 'commission_fn': commission_by_value})
+                       'commission_ratio': 0.0000, 'commission_fn': commission_by_value})
 
 # %% 热身
 print('warmup:', warmup())
@@ -67,8 +74,7 @@ with Timer():
 
 # %% 资产转换，只做一次即可
 df['asset'] = df['asset'].map(bt.mapping_asset_int)
-df = df.sort_values(by=['date', 'asset'])
-bt.run_bars(groupby(orders_daily(df, sort=False), by='date', dtype=order_outside_dt))
+bt.run_bars(groupby(orders_daily(df, sort=True), by='date', dtype=order_outside_dt))
 trades = bt.trades(return_all=True)
 print(trades)
 perf = bt.performances(return_all=True)
